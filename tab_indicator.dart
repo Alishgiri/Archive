1eb3221c255b95859dc2918b3fbfcfc4f7ae289d
Alish Giri <alishgiri@Alishs-MacBook-Pro.local>
2020-07-05 00:28:15 +0545

    Archive tab indicator widget

---
+++ tab_indicator.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class RoundedUnderlineIndicator extends Decoration {
+  final Color indicatorColor;
+  final double indicatorRadius;
+  final double indicatorHeight;
+  final EdgeInsetsGeometry insets;
+  final EdgeInsetsGeometry padding;
+  final TabBarIndicatorSize tabBarIndicatorSize;
+
+  const RoundedUnderlineIndicator({
+    this.indicatorHeight: 5.0,
+    this.indicatorRadius: 100.0,
+    this.indicatorColor: Colors.white,
+    this.tabBarIndicatorSize = TabBarIndicatorSize.label,
+    this.insets: const EdgeInsets.symmetric(horizontal: 5.0),
+    this.padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 2.0),
+  })  : assert(indicatorHeight != null),
+        assert(indicatorColor != null),
+        assert(indicatorRadius != null),
+        assert(padding != null),
+        assert(insets != null);
+
+  @override
+  Decoration lerpFrom(Decoration a, double t) {
+    if (a is RoundedUnderlineIndicator) {
+      return new RoundedUnderlineIndicator(
+        padding: EdgeInsetsGeometry.lerp(a.padding, padding, t),
+        insets: EdgeInsetsGeometry.lerp(a.insets, insets, t),
+      );
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  Decoration lerpTo(Decoration b, double t) {
+    if (b is RoundedUnderlineIndicator) {
+      return new RoundedUnderlineIndicator(
+        padding: EdgeInsetsGeometry.lerp(padding, b.padding, t),
+        insets: EdgeInsetsGeometry.lerp(insets, b.insets, t),
+      );
+    }
+    return super.lerpTo(b, t);
+  }
+
+  @override
+  _BubblePainter createBoxPainter([VoidCallback onChanged]) {
+    return new _BubblePainter(this, onChanged);
+  }
+}
+
+class _BubblePainter extends BoxPainter {
+  _BubblePainter(this.decoration, VoidCallback onChanged)
+      : assert(decoration != null),
+        super(onChanged);
+
+  final RoundedUnderlineIndicator decoration;
+
+  double get indicatorHeight => decoration.indicatorHeight;
+  Color get indicatorColor => decoration.indicatorColor;
+  double get indicatorRadius => decoration.indicatorRadius;
+  EdgeInsetsGeometry get padding => decoration.padding;
+  EdgeInsetsGeometry get insets => decoration.insets;
+  TabBarIndicatorSize get tabBarIndicatorSize => decoration.tabBarIndicatorSize;
+
+  Rect _indicatorRectFor(Rect rect, TextDirection textDirection) {
+    assert(rect != null);
+    assert(textDirection != null);
+
+    Rect indicator = padding.resolve(textDirection).inflateRect(rect);
+
+    if (tabBarIndicatorSize == TabBarIndicatorSize.tab) {
+      indicator = insets.resolve(textDirection).deflateRect(rect);
+    }
+
+    return new Rect.fromLTWH(
+      indicator.left,
+      indicator.bottom - indicator.height,
+      indicator.width,
+      indicator.height,
+    );
+  }
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    assert(configuration != null);
+    assert(configuration.size != null);
+    final Rect rect =
+        Offset(offset.dx, (configuration.size.height) - indicatorHeight) &
+            Size(configuration.size.width, indicatorHeight);
+    final TextDirection textDirection = configuration.textDirection;
+    final Rect indicator = _indicatorRectFor(rect, textDirection);
+    final Paint paint = Paint();
+    paint.color = indicatorColor;
+    paint.style = PaintingStyle.fill;
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(indicator, Radius.circular(indicatorRadius)),
+        paint);
+  }
+}
